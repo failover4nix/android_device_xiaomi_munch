5e739426210cbedc052c1c1d0e23ad30fecfc9a8
Andriannn <andrianp920@gmail.com>
2025-10-11 12:30:55 +0000

    munch: Use clang21 for kernel prebuilt
    Signed-off-by: Andriannn <andrianp920@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,19 @@
 cd kernel/xiaomi/munch
-git submodule update --init --remote --merge
-chmod +x nextpatch.sh && bash nextpatch.sh
+chmod +x nextpatch.sh &&  bash nextpatch.sh
 rm -rf KernelSU-Next/userspace/su
 cd ../../..
+# Define a variable for the target clang directory path to make the script cleaner
+CLANG_DIR="prebuilts/clang/host/linux-x86/clang-r574158"
+# Check if the clang directory does NOT exist
+if [ ! -d "$CLANG_DIR" ]; then
+  # If the directory is not found, print a message to the user
+  echo "Clang directory not found. Cloning..."
+  mkdir -p "$CLANG_DIR"
+  wget -qO- "https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/47e1fad3c1d7c647b49597e0ea85c5d8e43a978f/clang-r574158.tar.gz" | tar -xzf - -C "$CLANG_DIR"
+  
+  # Print a success message
+  echo "Clang has been downloaded and extracted successfully."
+else
+  # If the directory already exists, inform the user and skip the download
+  echo "Clang directory already exists. Skipping download."
+fi
